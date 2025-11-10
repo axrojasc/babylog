a82056aa722f99a58d5429b6a54a6094a03fa676
Maaxiimo <maxim.fuentess@duocuc.cl>
2025-11-09 23:18:12 -0300

    Vista vacunas y controles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'vacunas',
+    loadChildren: () =>
+      import('./pages/vacunas/vacunas.module').then(m => m.VacunasPageModule)
+  },
+  {
+    path: 'controles',
+    loadChildren: () =>
+      import('./pages/controles/controles.module').then(m => m.ControlesPageModule)
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
