0f09a62bcbbdbb6aa774034ed734b4821d3bdc41
Axel Rojas <ax.rojasc@duocuc.cl>
2025-11-15 01:37:06 -0300

    bottonback

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -11,11 +11,19 @@ export class HomePage {
   babyName = 'Aly';
   private readonly router = inject(Router);
 
+  goToSueno() {
+    this.router.navigate(['/sueno']);
+  }
+
   goToVacunas() {
     this.router.navigate(['/vacunas']);
   }
 
   goToControles() {
     this.router.navigate(['/controles']);
+  }
+
+  goToPeso() {
+    this.router.navigate(['/alimentacion']);
   }
 }
