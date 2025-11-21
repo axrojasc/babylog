47d9aca791109015a7730db04e09bc9ff48f0114
Francisco Iglesias <fra.iglesias@duocuc.cl>
2025-11-21 04:37:50 -0300

    Cambios de estilo general de app
    se realizan cambios de estilos en todas las vistas de la aplicacion

--- src/app/pages/main/crecimiento/crecimiento.module.ts
+++ src/app/pages/main/crecimiento/crecimiento.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CrecimientoPageRoutingModule } from './crecimiento-routing.module';
 import { CrecimientoPage } from './crecimiento.page';
+import { SharedModule } from 'src/app/shared/shared-module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, CrecimientoPageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, CrecimientoPageRoutingModule,SharedModule],
   declarations: [CrecimientoPage],
 })
-export class CrecimientoPageModule {}
+export class CrecimientoPageModule { }
